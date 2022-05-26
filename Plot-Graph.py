265e0aa485c8d1c78fec3a5d9f1a5a910e9b0f73
John <jfberry67@gmail.com>
2022-05-26 12:02:44 -0400

    Initial commit

---
+++ Plot-Graph.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('Current_IncVol_Cleaned.csv')
+
+print(df)
---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import tkinter as tk
+import os
+import platform
+from tkinter.filedialog import askopenfilename
+
+userhome = os.path.expanduser('~')
+desktop = 'C:\\Users\\jfber\\OneDrive\\Desktop\\'
+window = tk.Tk()
+window.withdraw()
+
+file_name = askopenfilename()
+#add path to roster csv file
+roster_df = pd.read_csv(file_name)
+
+data = pd.DataFrame(roster_df)
+
+
+data.drop(['StudMi', 'GrpGroupName', 'StaffLname',
+          'StaffFname', 'StaffMi', 'DormId', 'Resident', 'StudentGroupGroupId',
+           'StaffId', 'GrpGroupTypeCd', 'SortBy'], axis=1, inplace=True)
+# data['FullName'] = data['StudentFName'] + ' ' + data['StudentLName']
+# data.drop(['StudentFName', 'StudentLName'], axis=1, inplace=True)
+data = data.sort_values('BehaviorDt')
+data.to_csv(f"{desktop}Current_IncVol_Cleaned.csv")
